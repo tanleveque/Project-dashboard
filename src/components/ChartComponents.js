7b1889804b65c61188cecf2405fe0bf1ad06c8c4
DESKTOP-A3P70FI\Tanneguy <A01676152@itesm.mx>
2019-03-02 13:13:15 +0100

    adding charts

--- src/components/ChartComponents.js
+++ src/components/ChartComponents.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, Legend } from 'recharts';
+import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
 
 const series =
@@ -31,24 +31,25 @@ export default class ChartComponents extends Component {
         return (
 
             series.map(s => (
-                <LineChart
-                    width={500}
-                    height={300}
-                    data={s.data}
-                    key={s.serie}
-                    margin={{
-                        top: 5, right: 30, left: 20, bottom: 5,
-                    }}
-                >
+                <ResponsiveContainer height={400}>
+                    <LineChart
+                        height={200}
+                        data={s.data}
+                        key={s.serie}
+                        margin={{
+                            top: 5, right: 30, left: 20, bottom: 5,
+                        }}
+                    >
 
-                    <CartesianGrid strokeDasharray="3 3" />
-                    <XAxis dataKey="name" />
-                    <YAxis />
-                    <Tooltip />
-                    <Legend />
-                    <Line type="monotone" dataKey="France" stroke="#8884d8" activeDot={{ r: 8 }} />
-                    <Line type="monotone" dataKey="Espagne" stroke="#82ca9d" />
-                </LineChart>
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <XAxis dataKey="name" />
+                        <YAxis />
+                        <Tooltip />
+                        <Legend />
+                        <Line type="monotone" dataKey="France" stroke="#8884d8" activeDot={{ r: 8 }} />
+                        <Line type="monotone" dataKey="Espagne" stroke="#82ca9d" />
+                    </LineChart>
+                </ResponsiveContainer>
             ))
         );
     }
